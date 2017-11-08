f32fd1ee3d755b7f8d7b9c5bb54c7423d16dfb46
Dennis Kaikowski <Mullekular@Denniss-MacBook-Pro.local>
2017-11-08 15:01:14 +0100

    nodemon hinzugefügt -test pug online unter ..:3000/test

--- routes/test.js
+++ routes/test.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 /* GET test page. */
 router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Testseite' });
-});
-
-router.get('/user', function(req, res, next) {
-  res.send('respond with a resource');
 });
 
 module.exports = router;
