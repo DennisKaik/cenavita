202a9e998e2ce3dff0d0c39c102ca9dc24a18aab
Steffen Wohlers <steffenwohlers@gmx.de>
2017-11-27 10:05:39 +0100

    port, angluar in package, db angepasst

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./api/routes');
 
 // Define the port to run on
 app.set('port', 3000);
+app.set('port', (process.env.PORT || 5000));
 
 // Add middleware to console log every request
 app.use(function(req, res, next) {
